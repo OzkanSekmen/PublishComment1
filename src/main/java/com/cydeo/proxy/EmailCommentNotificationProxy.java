a60dc710bfbb610e969bafdb8554d4a90ba9f093
Ozkan Sekmen <sekmenozkan@hotmail.com>
2023-10-15 15:10:47 +0300

    initial classes.

--- src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
+++ src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
@@ -7,6 +7,6 @@ public class EmailCommentNotificationProxy implements CommentNotificationProxy {
 
     @Override
     public void sendComment(Comment comment) {
-        System.out.println("Sending notification for comment");
+        System.out.println("Sending notification for comment: "+ comment.getText());
     }
 }
